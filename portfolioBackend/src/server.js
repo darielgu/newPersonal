aaf8e96bbebf08d5f1a9428c03e807a47cc8f6e4
Dariel <darielgutierrez332@gmail.com>
2025-08-19 16:06:57 -0700

    initialized simple backend!

--- portfolioBackend/src/server.js
+++ portfolioBackend/src/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
-const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
