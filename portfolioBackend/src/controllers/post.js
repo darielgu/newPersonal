585bcecf22bb92b5f0e0fe7971dd96492355c215
Dariel <darielgutierrez332@gmail.com>
2025-08-21 14:08:32 -0700

    Backend is running - use npm start

--- portfolioBackend/src/controllers/post.js
+++ portfolioBackend/src/controllers/post.js
@@ -1,5 +1,5 @@
 // Controller is how we handle the logic for interacting with our DB or fetching
-import prisma from "../db/db";
+import prisma from "../db/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -13,6 +13,7 @@ export const createPost = async (req, res) => {
       data: {
         title,
         content,
+        // connect is how we relate the post to the user
         user: {
           connect: { id: process.env.USER_ID },
         },
@@ -21,5 +22,52 @@ export const createPost = async (req, res) => {
     res.status(201).json(newPost);
   } catch (error) {
     res.status(500).json({ error: "Failed to create post" });
+  }
+};
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany({});
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve posts" });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  try {
+    const updatedPost = await prisma.post.update({
+      where: { id: Number(id) },
+      data: { title, content },
+    });
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update post" });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: Number(id) },
+    });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve post" });
+  }
+};
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.post.delete({
+      where: { id: Number(id) },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete post" });
   }
 };
--- portfolioBackend/src/routes/postRoutes.js
+++ portfolioBackend/src/routes/postRoutes.js
@@ -2,14 +2,17 @@
 import express from "express";
 import {
   getPosts,
+  getPostById,
   createPost,
   updatePost,
   deletePost,
 } from "../controllers/post.js";
+import prisma from "../db/db.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/:id", getPostById);
 router.post("/", createPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
